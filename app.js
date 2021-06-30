2bcee8dbfd3eab2be301481393880365f1b8d94e
Julie Nisbet <jmnisbet@gmail.com>
2021-06-30 11:56:12 -0700

    adding comments for guess-a-number

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const waterBtn = document.getElementById('water-btn');
 // initialize state
 let waterLevel = 0;
 
+//  STATE for guess-a-number
+// const userGuess --> get this from the input field
+// const targetNumber --> get this using Math module
+// const guessesRemaining = 4
+
+
 // set event listeners 
   // get user input
   // use user input to update state 
@@ -22,6 +28,21 @@ function renderPlant(plantString){
     plantFeedback.textContent = `The plant is ${plantString}! ${waterLevel}`;
 }
   // add event listener to button click
+
+//   EVENT LISTENERS FOR GUESS A NUMBER
+// on button click
+//   get the userGuess from the input -- convert it to a number
+//   check the userGuess against the targetNumber
+//   if userGuess === targetNumber --> 
+//       END THE GAME
+//       display winning message
+//   if userGuess !== targetNumber --> 
+//       display feedback with over or under
+//       decrement remaining guesses
+//          if no guesses remaining
+//             END THE GAME
+//             display losing message
+
 waterBtn.addEventListener('click', ()=>{
     
     // increment the waterLevel
